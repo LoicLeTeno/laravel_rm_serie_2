2b8970d846d64aa4041c5ab1feb079bd8f396275
le Téno <leteno.loic@gmail.com>
2021-07-19 16:31:23 +0200

    fin exo 5 LLT

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/config', function () {
+    return view('config');
+})->name('config');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
